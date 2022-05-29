d986e39046cb4293226a3771771af57548f8c565
m-aiken <mattaiken91@gmail.com>
2022-05-29 14:51:05 +0100

    PFM12-22 reshaped meter bounds to make space for labels

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -115,8 +115,13 @@ void Meter::paint(juce::Graphics& g)
 {
     auto bounds = getLocalBounds();
     auto componentHeight = bounds.getHeight();
-    
-    g.fillAll(juce::Colour(250u, 243u, 221u)); // background
+    auto maxMeterHeight = componentHeight * 0.95;
+    auto meterBoundsYOffset = componentHeight - maxMeterHeight;
+    g.fillAll(juce::Colours::black); // background
+    auto maxMeterBounds = juce::Rectangle<int>(0,
+                                               meterBoundsYOffset,
+                                               bounds.getWidth(),
+                                               maxMeterHeight);
     
     // Peak Meter
     g.setColour(juce::Colour(82u, 182u, 154u));
@@ -125,10 +130,10 @@ void Meter::paint(juce::Graphics& g)
                                         NEGATIVE_INFINITY,
                                         MAX_DECIBELS,
                                         componentHeight,
-                                        0);
+                                        meterBoundsYOffset);
     
-    g.fillRect(bounds
-                .withHeight(componentHeight * peakScaled)
+    g.fillRect(maxMeterBounds
+                .withHeight(maxMeterHeight * peakScaled)
                 .withY(peakScaled));
     
     // Average Meter
@@ -138,29 +143,43 @@ void Meter::paint(juce::Graphics& g)
                                            NEGATIVE_INFINITY,
                                            MAX_DECIBELS,
                                            componentHeight,
-                                           0);
+                                           meterBoundsYOffset);
     
-    auto avgMeterWidth = bounds.getWidth() * 0.6;
-    g.fillRect(bounds
-                .withHeight(componentHeight * averageScaled)
+    auto avgMeterWidth = bounds.getWidth() * 0.5;
+    g.fillRect(maxMeterBounds
+                .withHeight(maxMeterHeight * averageScaled)
                 .withY(averageScaled)
                 .withWidth(avgMeterWidth)
                 .withX(bounds.getCentreX() - (avgMeterWidth * 0.5)));
     
     // Falling Tick
-    g.setColour( fallingTick.isOverThreshold() ? juce::Colours::red : juce::Colours::orange );
+    g.setColour( fallingTick.isOverThreshold() ? juce::Colours::red : juce::Colours::white );
     
     auto tickValueScaled = juce::jmap<float>(fallingTick.getCurrentValue(),
                                              NEGATIVE_INFINITY,
                                              MAX_DECIBELS,
-                                             bounds.getHeight(),
-                                             0);
+                                             componentHeight,
+                                             meterBoundsYOffset);
             
     g.drawLine(bounds.getX(),     // startX
                tickValueScaled,   // startY
                bounds.getRight(), // endX
                tickValueScaled,   // endY
                3.f);              // line thickness
+    
+    /*
+    // L/R label
+    g.drawFittedText(<#const String &text#>,
+                     <#int x#>,
+                     <#int y#>,
+                     <#int width#>,
+                     <#int height#>,
+                     <#Justification justificationFlags#>,
+                     <#int maximumNumberOfLines#>)
+    */
+    // Meter bounding box/line
+    g.setColour(juce::Colours::white);
+    g.drawRect(maxMeterBounds);
 }
 
 void Meter::update(const float& dbLevel)
