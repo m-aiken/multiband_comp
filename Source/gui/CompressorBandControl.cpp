02d776aa35972d44b93cadeaf920d1a98d8b3e2e
m-aiken <mattaiken91@gmail.com>
2022-06-15 13:35:17 +0100

    PFM12-28 added band retargeting for rotary controls

--- Source/gui/CompressorBandControl.cpp
+++ Source/gui/CompressorBandControl.cpp
@@ -16,29 +16,45 @@
 CompressorBandControl::CompressorBandControl(juce::AudioProcessorValueTreeState& _apvts)
     : apvts(_apvts)
 {
-    auto initRotaryControl = [&apvts = this->apvts](auto& rotaryControl, const auto& paramName, const auto& suffix, const auto& bandNum)
+    auto selectedBand = getSelectedBand();
+    auto initRotaryControl = [&](auto& rotaryControl, const auto& paramName, const auto& suffix)
     {
-        auto param = apvts.getParameter(Params::getBandControlParamName(paramName, bandNum));
+        auto param = apvts.getParameter(Params::getBandControlParamName(paramName, selectedBand));
         jassert( param != nullptr );
         
         rotaryControl = std::make_unique<RotaryControl>(*param, suffix, Params::bandControlMap.at(paramName));
     };
     
-    initRotaryControl(attackRotary,     Params::BandControl::Attack,    "ms", 0);
-    initRotaryControl(releaseRotary,    Params::BandControl::Release,   "ms", 0);
-    initRotaryControl(thresholdRotary,  Params::BandControl::Threshold, "dB", 0);
-    initRotaryControl(makeupGainRotary, Params::BandControl::Gain,      "dB", 0);
-    initRotaryControl(ratioRotary,      Params::BandControl::Ratio,     "",   0);
-    
-    attackAttachment     = std::make_unique<Attachment>(apvts, Params::getBandControlParamName(Params::BandControl::Attack, 0),    *attackRotary);
-    releaseAttachment    = std::make_unique<Attachment>(apvts, Params::getBandControlParamName(Params::BandControl::Release, 0),   *releaseRotary);
-    thresholdAttachment  = std::make_unique<Attachment>(apvts, Params::getBandControlParamName(Params::BandControl::Threshold, 0), *thresholdRotary);
-    makeupGainAttachment = std::make_unique<Attachment>(apvts, Params::getBandControlParamName(Params::BandControl::Gain, 0),      *makeupGainRotary);
-    ratioAttachment      = std::make_unique<Attachment>(apvts, Params::getBandControlParamName(Params::BandControl::Ratio, 0),     *ratioRotary);
+    initRotaryControl(attackRotary,     Params::BandControl::Attack,    "ms");
+    initRotaryControl(releaseRotary,    Params::BandControl::Release,   "ms");
+    initRotaryControl(thresholdRotary,  Params::BandControl::Threshold, "dB");
+    initRotaryControl(makeupGainRotary, Params::BandControl::Gain,      "dB");
+    initRotaryControl(ratioRotary,      Params::BandControl::Ratio,     "");
     
     resetButton.setColour(juce::TextButton::ColourIds::buttonColourId, ColourPalette::getColour(ColourPalette::Red));
     resetButton.setColour(juce::TextButton::ColourIds::textColourOffId, ColourPalette::getColour(ColourPalette::Text));
     resetButton.onClick = [this]() { resetParamsToDefault(); };
+    
+    const auto& params = Params::getParams();
+    auto selectedBandParam = dynamic_cast<juce::RangedAudioParameter*>(apvts.getParameter(params.at(Params::Names::Selected_Band)));
+    jassert( selectedBandParam != nullptr );
+    selectedBandListener = std::make_unique<ParamListener<float>>(*selectedBandParam, [this](const auto& parameterValue){ updateAttachments(); });
+    
+    auto mbsCallback = [this](const float& selectedBand){ updateEnablements(); };
+    for ( auto i = 0; i < Globals::getNumMaxBands(); ++i )
+    {
+        auto muteParam = dynamic_cast<juce::RangedAudioParameter*>(apvts.getParameter(Params::getBandControlParamName(Params::BandControl::Mute, i)));
+        auto soloParam = dynamic_cast<juce::RangedAudioParameter*>(apvts.getParameter(Params::getBandControlParamName(Params::BandControl::Solo, i)));
+        auto bypassParam = dynamic_cast<juce::RangedAudioParameter*>(apvts.getParameter(Params::getBandControlParamName(Params::BandControl::Bypass, i)));
+        
+        jassert( muteParam != nullptr && soloParam != nullptr && bypassParam != nullptr);
+        
+        muteListeners.push_back(std::make_unique<ParamListener<float>>(*muteParam, mbsCallback));
+        soloListeners.push_back(std::make_unique<ParamListener<float>>(*soloParam, mbsCallback));
+        bypassListeners.push_back(std::make_unique<ParamListener<float>>(*bypassParam, mbsCallback));
+    }
+    
+    updateAttachments();
     
     addAndMakeVisible(*attackRotary);
     addAndMakeVisible(*releaseRotary);
@@ -47,6 +63,7 @@ CompressorBandControl::CompressorBandControl(juce::AudioProcessorValueTreeState&
     addAndMakeVisible(*ratioRotary);
     
     addAndMakeVisible(resetButton);
+    addAndMakeVisible(bandNumButton);
 }
 
 void CompressorBandControl::paint(juce::Graphics& g)
@@ -80,12 +97,19 @@ void CompressorBandControl::resized()
     
     grid.performLayout(rotaryBounds);
     
-    // Button
-    auto buttonSize = bounds.getWidth() * 0.06;
-    resetButton.setBounds(bounds.getRight() - buttonSize - 5,
-                          bounds.getCentreY() - (buttonSize * 0.5),
-                          buttonSize,
-                          buttonSize);
+    // Reset button
+    auto resetButtonSize = bounds.getWidth() * 0.06;
+    resetButton.setBounds(bounds.getRight() - resetButtonSize - 5,
+                          bounds.getCentreY() - (resetButtonSize * 0.5),
+                          resetButtonSize,
+                          resetButtonSize);
+    
+    // Label (button)
+    auto labelButtonSize = resetButtonSize * 0.5;
+    bandNumButton.setBounds(bounds.getRight() - labelButtonSize - 5,
+                            5,
+                            labelButtonSize,
+                            labelButtonSize);
 }
 
 void CompressorBandControl::resetHelper(const juce::String& paramName)
@@ -105,9 +129,58 @@ void CompressorBandControl::resetHelper(juce::RangedAudioParameter* param, const
 
 void CompressorBandControl::resetParamsToDefault()
 {
-    resetHelper(Params::getBandControlParamName(Params::BandControl::Attack, 0));
-    resetHelper(Params::getBandControlParamName(Params::BandControl::Release, 0));
-    resetHelper(Params::getBandControlParamName(Params::BandControl::Threshold, 0));
-    resetHelper(Params::getBandControlParamName(Params::BandControl::Gain, 0));
-    resetHelper(Params::getBandControlParamName(Params::BandControl::Ratio, 0));
+    auto selectedBand = getSelectedBand();
+    resetHelper(Params::getBandControlParamName(Params::BandControl::Attack,    selectedBand));
+    resetHelper(Params::getBandControlParamName(Params::BandControl::Release,   selectedBand));
+    resetHelper(Params::getBandControlParamName(Params::BandControl::Threshold, selectedBand));
+    resetHelper(Params::getBandControlParamName(Params::BandControl::Gain,      selectedBand));
+    resetHelper(Params::getBandControlParamName(Params::BandControl::Ratio,     selectedBand));
+}
+
+void CompressorBandControl::updateAttachments()
+{
+    auto selectedBand = getSelectedBand();
+    bandNumButton.setButtonText(juce::String(selectedBand + 1));
+    
+    attackAttachment.reset();
+    releaseAttachment.reset();
+    thresholdAttachment.reset();
+    makeupGainAttachment.reset();
+    ratioAttachment.reset();
+    
+    attackAttachment     = std::make_unique<Attachment>(apvts, Params::getBandControlParamName(Params::BandControl::Attack,    selectedBand), *attackRotary);
+    releaseAttachment    = std::make_unique<Attachment>(apvts, Params::getBandControlParamName(Params::BandControl::Release,   selectedBand), *releaseRotary);
+    thresholdAttachment  = std::make_unique<Attachment>(apvts, Params::getBandControlParamName(Params::BandControl::Threshold, selectedBand), *thresholdRotary);
+    makeupGainAttachment = std::make_unique<Attachment>(apvts, Params::getBandControlParamName(Params::BandControl::Gain,      selectedBand), *makeupGainRotary);
+    ratioAttachment      = std::make_unique<Attachment>(apvts, Params::getBandControlParamName(Params::BandControl::Ratio,     selectedBand), *ratioRotary);
+    
+    updateEnablements();
+}
+
+void CompressorBandControl::updateEnablements()
+{
+    auto selectedBand = getSelectedBand();
+    
+    auto muteParam = dynamic_cast<juce::RangedAudioParameter*>(apvts.getParameter(Params::getBandControlParamName(Params::BandControl::Mute, selectedBand)));
+    auto bypassParam = dynamic_cast<juce::RangedAudioParameter*>(apvts.getParameter(Params::getBandControlParamName(Params::BandControl::Bypass, selectedBand)));
+    jassert( muteParam != nullptr && bypassParam != nullptr);
+    
+    auto isMutedOrBypassed = ( muteParam->getValue() == 1 || bypassParam->getValue() == 1 );
+    
+    attackRotary->setEnabled     (!isMutedOrBypassed);
+    releaseRotary->setEnabled    (!isMutedOrBypassed);
+    thresholdRotary->setEnabled  (!isMutedOrBypassed);
+    makeupGainRotary->setEnabled (!isMutedOrBypassed);
+    ratioRotary->setEnabled      (!isMutedOrBypassed);
+    
+    setAlpha(isMutedOrBypassed ? 0.5f : 1.f);
+}
+
+int CompressorBandControl::getSelectedBand()
+{
+    const auto& params = Params::getParams();
+    auto selectedBandParam = dynamic_cast<juce::RangedAudioParameter*>(apvts.getParameter(params.at(Params::Names::Selected_Band)));
+    jassert( selectedBandParam != nullptr );
+        
+    return selectedBandParam->convertFrom0to1(selectedBandParam->getValue());
 }
